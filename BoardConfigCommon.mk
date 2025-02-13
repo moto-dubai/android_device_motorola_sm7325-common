a2e2d26ebf8a318e917b590c4fd1d0d43b5b0ac8
Sevenrock <sevenrock@hotmail.de>
2025-02-13 22:25:28 +0100

    Revert "sm8250-common: add a framework manifest file"
    This reverts commit f1ae7f82840f61cf4da913bdc063fea27b723585.
    
    Remove remnants of qccsyshal.
    
    Change-Id: If47d3df68bb02c497b5b70e49e347bcdee8c7efb

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -108,7 +108,6 @@ TARGET_FS_CONFIG_GEN := $(COMMON_PATH)/config.fs
 BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := default
 
 # HIDL
-DEVICE_FRAMEWORK_MANIFEST_FILE += $(COMMON_PATH)/framework_manifest.xml
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     $(COMMON_PATH)/device_framework_matrix.xml \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
